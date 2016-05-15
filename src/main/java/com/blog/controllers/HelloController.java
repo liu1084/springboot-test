0d9edd32134833f5bef2df54076374e14026b06f
Jim Liu <liu1084@163.com>
2016-05-16 03:44:51 +0800

    controller

--- src/main/java/com/blog/controllers/HelloController.java
+++ src/main/java/com/blog/controllers/HelloController.java
@@ -1,17 +1,34 @@
 package com.blog.controllers;
 
+import com.blog.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by liqing on 2016/5/15.
  */
-@Controller
-@RequestMapping(value = "/hello")
+@RestController
+@RequestMapping(value = {"/", "/index", "", ".html", ".htm"})
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String world(){
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String world() {
         return "world";
+    }
+
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public User user(@PathVariable(value = "name") String name) {
+        User user = new User();
+        user.setName(name);
+        return user;
+    }
+
+    @RequestMapping("/mv")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("mv");
+        return mv;
     }
 }
---
+++ src/main/java/com/blog/models/User.java
@@ -0,0 +1,16 @@
+package com.blog.models;
+
+/**
+ * Created by liqing on 2016/5/16.
+ */
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
